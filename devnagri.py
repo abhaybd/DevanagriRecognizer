5b54ba94ef550d57c0552802528c1a5490c53485
Abhay D <a.deshpande012@gmail.com>
2017-08-26 11:00:46 -0700

    Deleted it all, switching to maven.

--- devnagri.py
+++ devnagri.py
@@ -2,7 +2,7 @@
 
 # Import libraries
 from keras.models import Sequential
-from keras.layers import Conv2D
+from keras.layers import Convolution2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
@@ -11,7 +11,7 @@ from keras.layers import Dropout
 classifier = Sequential()
 
 # Add convolution layer
-classifier.add(Conv2D(filters=25, kernel_size=(3,3), input_shape=(32, 32, 1), activation='relu'))
+classifier.add(Convolution2D(25, 3,3, input_shape=(32, 32, 1), activation='relu'))
 
 # Pooling
 classifier.add(MaxPooling2D(pool_size=(2,2)))
@@ -20,9 +20,9 @@ classifier.add(MaxPooling2D(pool_size=(2,2)))
 classifier.add(Flatten())
 
 # Add full connection
-classifier.add(Dense(units=128, activation='relu'))
-classifier.add(Dropout(rate=0.1))
-classifier.add(Dense(units=36, activation='softmax'))
+classifier.add(Dense(output_dim=128, activation='relu'))
+classifier.add(Dropout(p=0.1))
+classifier.add(Dense(output_dim=36, activation='softmax'))
 
 # Compiling the ANN
 classifier.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
@@ -50,9 +50,10 @@ test_set = test_datagen.flow_from_directory(
 
 classifier.fit_generator(
         train_set,
-        steps_per_epoch=61200,
-        epochs=25,
+        samples_per_epoch=61200,
+        nb_epoch=25,
         validation_data=test_set,
-        validation_steps=10800)
+        nb_val_samples=10800)
 
-classifier.save('devanagri.h5')
+classifier.save('model.h5')
+
--- src/com/coolioasjulio/devnagri/ModelImporter.java
+++
@@ -1,7 +0,0 @@
-package com.coolioasjulio.devnagri;
-
-import java.io.IOException;
-
-public class ModelImporter {
-	//TODO create importModel method
-}
