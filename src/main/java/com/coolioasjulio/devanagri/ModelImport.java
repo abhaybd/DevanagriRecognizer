e4f52f155d68713fe793db75088bae7a88f0f03b
Abhay D <a.deshpande012@gmail.com>
2018-03-19 12:11:58 -0700

    Update .gitignore, added save to model import

--- src/main/java/com/coolioasjulio/devanagri/ModelImport.java
+++ src/main/java/com/coolioasjulio/devanagri/ModelImport.java
@@ -4,19 +4,19 @@ import org.deeplearning4j.nn.modelimport.keras.KerasModelImport;
 import org.deeplearning4j.nn.modelimport.keras.exceptions.InvalidKerasConfigurationException;
 import org.deeplearning4j.nn.modelimport.keras.exceptions.UnsupportedKerasConfigurationException;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.util.ModelSerializer;
 
 import java.io.IOException;
 
 public class ModelImport {
     public static void main(String[] args) throws IOException {
-        importModel("model_complex.h5");
+        MultiLayerNetwork model = importModel("model_complex.h5");
+        ModelSerializer.writeModel(model, "model.zip", false);
     }
     public static MultiLayerNetwork importModel(String path) throws IOException{
         try {
             return KerasModelImport.importKerasSequentialModelAndWeights(path);
-        } catch (UnsupportedKerasConfigurationException e) {
-            e.printStackTrace();
-        } catch (InvalidKerasConfigurationException e) {
+        } catch (UnsupportedKerasConfigurationException | InvalidKerasConfigurationException e) {
             e.printStackTrace();
         }
         throw new IOException("Failed to load model!");
